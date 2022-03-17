050ed54dc50b0c027c3f1c5d1bce41d8e4b7dad7
sahactys <895004562613ds@gmail.com>
2022-03-17 21:59:16 +0300

    The beginning has been laid for autotests

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,18 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun main() {
+    }
+
+    @Test
+    fun calculatingTheCommission() {
+    }
+
+    @Test
+    fun output() {
+    }
+}
