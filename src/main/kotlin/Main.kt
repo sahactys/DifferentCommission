c1dccac24205f884a773b50fdf5d15b2013d665a
sahactys <895004562613ds@gmail.com>
2022-03-20 14:55:49 +0300

    broken commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ enum class PaymentSystem {
 const val CONSTANT_COMMISSION: Int = 2000
 const val MIN_COMMISSION: Int = 3500
 
-fun main() {
+fun main(){
     var paymentSystemChoice: Int
     var sum: Int
     val sumPastTransfer=0
@@ -72,7 +72,7 @@ fun calculatingTheCommission(
 fun output(sum: Int, commission: Int = 0):String {
     return if (commission == 0) {
         ("Комиссия не взымается \n" +
-                "Сумма перевода " + sum / 100 + "руб. " + sum % 100 + " коп.")
+                "Сумма перевода " + sum / 1000 + "руб. " + sum % 100 + " коп.")
     } else {
         ("Комиссия " + commission / 100 + "руб. " + commission % 100 + " коп. \n" +
                 "Сумма перевода " + (sum - commission) / 100 + "руб. " + (sum - commission) % 100 + "коп.")
